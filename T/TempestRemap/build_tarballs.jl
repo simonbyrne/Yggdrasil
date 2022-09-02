0322189b8d8e1c24d69bf441a445f56c8f5c6533
Simon Byrne <simonbyrne@gmail.com>
2022-09-02 14:26:05 -0700

    TempestRemap: support more platforms

--- T/TempestRemap/build_tarballs.jl
+++ T/TempestRemap/build_tarballs.jl
@@ -36,11 +36,7 @@ install_license ../LICENSE
 """
 
 # Note: We are restricted to the platforms that NetCDF supports, the library is Unix only
-platforms = [
-    Platform("x86_64", "linux"),
-    Platform("aarch64", "linux"; libc="glibc"),
-    Platform("x86_64", "macos"),
-] 
+platforms = supported_platforms()
 platforms = expand_cxxstring_abis(platforms)
 
 products = [
